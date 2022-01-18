8dd313b7592284066f64daaf6e6b52d9f984bf05
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2022-01-18 18:57:04 +0530

    pushing new projects

---
+++ ViewDemo/ViewDemo/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult ConversionDemo()
+        {
+            return View();
+        }
+
+        public ActionResult ConditionalLogicDemo()
+        {
+            return View();
+        }
+
+        public ActionResult LadderIfDemo()
+        {
+            return View();
+        }
+
+        public ActionResult SwitchDemo()
+        {
+            return View();
+        }
+
+        public ActionResult LoopDemo()
+        {
+            return View();
+        }
+    }
+}
